c0b1ac210df67832313813459fe74c9cf9f2f741
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-02-16 17:58:27 -0500

    Allowing npm install to run as root

--- provisioners/node.sh
+++ provisioners/node.sh
@@ -23,6 +23,6 @@ npm install bower -g --loglevel=error
 
 echo "[accelerated/provisioners/node.sh] -- Installing application"
 
-npm install --loglevel=error > /dev/null
+npm install --loglevel=error --unsafe-perm > /dev/null
 
 echo "[accelerated/provisioners/node.sh] -- Finished installing!"
